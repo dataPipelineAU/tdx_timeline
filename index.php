65d28aa5a7b90e885ff5cb4822876f83be951660
Robert Layton <robertlayton@gmail.com>
2017-02-13 14:11:40 +1100

    Link to swimlane and directly to  a useful timeline

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <h3><a href="uploader.php">Uploader</a></h3>
-<h3><a href="timelines.php">Timeline Viewer</a></h3>
+<h3><a href="timelines.php?external_key=train">Timeline Viewer</a></h3>
+<h3><a href="swimlane.php?external_key=swimlane">Swimlane Viewer</a></h3>
